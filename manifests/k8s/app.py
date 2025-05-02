658252f4633a9f699a0e5bcc93daaf3148ff1e07
diego <diego.ormeno@ayi.group>
2025-05-01 21:37:12 -0300

    first commit

--- manifests/k8s/app.py
+++ manifests/k8s/app.py
@@ -1,13 +1,18 @@
 from flask import Flask, request
 import logging
 
+import os
+from datetime import datetime
+
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
-@app.route("/")
-def hello():
-    app.logger.info("Hello endpoint was called from %s", request.remote_addr)
-    return "Hello from Python app with logs!\n"
+@app.route('/')
+def index():
+    path = '/app/data/visits.log'
+    with open(path, 'a') as f:
+        f.write(f"Visit at {datetime.now()}\n")
+    return "Hello from Flask with persistence!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
